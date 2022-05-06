bd4b55d364219102c8d0e22c958cdc1b339c59f1
Felipe Alexandre Gasparelo <gaspawn@gmail.com>
2022-05-06 11:15:33 -0300

    django-on-heroku install

--- controleatendimentos/settings.py
+++ controleatendimentos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ REST_FRAMEWORK = {
 
 #configurações para usar model Pessoa como user de logon
 AUTH_USER_MODEL = 'agendamentos.Pessoa'
+
+django_on_heroku.settings(locals())
