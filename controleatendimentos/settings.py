40904ebbb18c41dd1d91f35523cb19b2c134230d
André Passos <63965710+andrepassosb@users.noreply.github.com>
2022-09-12 13:41:16 -0300

    Update settings.py

--- controleatendimentos/settings.py
+++ controleatendimentos/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'controleatendimentos.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
 
 
