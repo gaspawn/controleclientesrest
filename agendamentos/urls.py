01d51589afec7eeda3a19a5057ce2e1fb234c441
Felipe Alexandre Gasparelo <gaspawn@gmail.com>
2022-10-15 19:42:45 -0300

    inclusao do swagger

--- agendamentos/urls.py
+++ agendamentos/urls.py
@@ -1,8 +1,27 @@
-from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.urls import path, include, re_path
 from rest_framework import routers
 from agendamentos.api.viewsets import PessoaViewSet, AgendamentoViewSet, ServicoViewSet, HistoricoPontosPessoaViewSet, SaldoPontosManager
 from rest_framework_simplejwt import views as jwt_views
 from agendamentos.util import token
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 router = routers.DefaultRouter()
 
@@ -22,5 +41,8 @@ urlpatterns = [
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
     path('', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
--- controleatendimentos/settings.py
+++ controleatendimentos/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'drf_yasg',
     'agendamentos'
 ]
 
